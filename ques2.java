dc0bab51ae660835bf7d3e9899578fa81973a7c5
EshaanSandhu <110472900+EshaanSandhu@users.noreply.github.com>
2023-06-07 07:20:59 +0530

    Add files via upload

---
+++ ques2.java
@@ -0,0 +1,8 @@
+class ques2{
+public static void main(String args[]){
+double a =94.64;
+int intportion = (int)a;
+System.out.println("Double value is:"+a);
+System.out.println(" Int value is:"+intportion);
+System.out.println("Decimal value is:"+(a-intportion));
+}}
